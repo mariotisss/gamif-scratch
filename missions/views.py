cd69fb86ca5fb79fb595e1fdaf237d6ac8677afc
uo283406 <uo283406@uniovi.es>
2025-04-08 00:20:44 +0200

    crear mision y poner admin

--- missions/views.py
+++ missions/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Mission
 from .serializers import MissionSerializer
 
-# Create your views here.
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        # SAFE_METHODS = GET, HEAD, OPTIONS
+        # SAFE_METHODS = GET, HEAD, OPTIONS --> Permite lectura a cualquier usuario
         if request.method in permissions.SAFE_METHODS:
             return True
+        # Permite escritura solo a usuarios administradores
+        # (is_staff es un atributo de User que indica si el usuario es admin)
         return request.user and request.user.is_staff
 
 class MissionViewSet(viewsets.ModelViewSet):
